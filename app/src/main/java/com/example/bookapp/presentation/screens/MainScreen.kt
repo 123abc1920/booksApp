7f8171122cfa76fd3c7cbecd02c8eea8eeeb234a
123abc1920 <123abc1920@mail.ru>
2025-03-20 00:59:28 +0300

    add recycle view

--- app/src/main/java/com/example/bookapp/presentation/screens/MainScreen.kt
+++ app/src/main/java/com/example/bookapp/presentation/screens/MainScreen.kt
@@ -2,10 +2,15 @@ package com.example.bookapp.presentation.screens
 
 import android.util.Log
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyItemScope
 import androidx.compose.material3.Button
+import androidx.compose.material3.ListItem
 import androidx.compose.material3.TextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -16,10 +21,12 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.example.bookapp.data.remotebooks.Consts
 import com.example.bookapp.data.remotebooks.Service
 import com.example.bookapp.data.remotebooks.models.BooksResponse
+import com.example.bookapp.data.remotebooks.models.Volume
 import com.example.bookapp.presentation.theme.BookAppTheme
 import retrofit2.Call
 import retrofit2.Callback
@@ -30,6 +37,16 @@ lateinit var mService: Service
 @Composable
 fun MainScreen(navController: NavController) {
     var text by remember { mutableStateOf("") }
+    var volumess by remember { mutableStateOf<List<Volume>?>(null) }
+    mService = Consts.service
+
+    getAllBooksList("Hary") { volumes ->
+        if (volumes != null) {
+            volumess = volumes
+        } else {
+        }
+    }
+
     BookAppTheme {
         Column {
             Column {
@@ -39,14 +56,12 @@ fun MainScreen(navController: NavController) {
                     label = { Text("Поиск") },
                     modifier = Modifier.fillMaxWidth()
                 )
-                Text(
-                    text = "Введите название книги",
-                    modifier = Modifier
-                        .fillMaxHeight(0.8f)
-                        .fillMaxWidth(),
-                    textAlign = TextAlign.Center
-                )
             }
+            Column(
+                content = {
+                    volumess?.let { BookListItem(it) }
+                }
+            )
             Row(
                 modifier = Modifier.fillMaxWidth(),
                 verticalAlignment = Alignment.Bottom
@@ -66,14 +81,28 @@ fun MainScreen(navController: NavController) {
             }
         }
     }
-
-    mService = Consts.service
-    //getAllBooksList("happy")
 }
 
-private fun getAllBooksList(target: String) {
+@Composable
+fun BookListItem(volumes: List<Volume>) {
+    LazyColumn(
+        contentPadding = PaddingValues(horizontal = 16.dp, vertical = 8.dp)
+    ) {
+        items(volumes.size) { index ->
+            Row {
+                Column {
+                    Text(text = volumes.get(index).volumeInfo.title)
+                    Text(text = volumes.get(index).volumeInfo.imageLinks.toString())
+                }
+            }
+        }
+    }
+}
+
+private fun getAllBooksList(target: String, callback: (List<Volume>?) -> Unit) {
     mService.getBooksList(target).enqueue(object : Callback<BooksResponse> {
         override fun onFailure(call: Call<BooksResponse>, t: Throwable) {
+            callback(null)
         }
 
         override fun onResponse(
@@ -82,8 +111,9 @@ private fun getAllBooksList(target: String) {
         ) {
             if (response.isSuccessful) {
                 val books: BooksResponse? = response.body()
-                Log.i("INFO", "$books")
+                callback(books?.items)
             } else {
+                callback(null)
             }
         }
     })
